14096c498812c83390a636fa5f66e91ebb76bf60
Yuri Solyanik <jura.s.n@gmail.com>
2025-09-17 19:29:45 +0500

    Начало разработки

---
+++ Source/LeaveMeAlone/MyGameModeBase.h
@@ -0,0 +1,16 @@
+// LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAVEMEALONE_API AMyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()	
+};
